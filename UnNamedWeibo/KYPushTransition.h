507ea92fb06118c3fe75fbaae68b0894c14756f9
KittenYang <kittenyang@icloud.com>
2015-04-18 21:33:51 +0800

    updated

---
+++ UnNamedWeibo/KYPushTransition.h
@@ -0,0 +1,13 @@
+//
+//  KYPushTransition.h
+//  UnNamedWeibo
+//
+//  Created by Kitten Yang on 4/18/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KYPushTransition : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
